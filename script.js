13af6851daa095e0f21e22fd58e8e1cc8bd95c14
VladBrok <vlad.brok99@gmail.com>
2023-07-25 15:27:23 +0300

    clear also cache, downloads, etc.

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-chrome.runtime.onStartup.addListener(deleteHistory);
+chrome.runtime.onStartup.addListener(deleteData);
 
 chrome.windows.onCreated.addListener(async () => {
   const windows = await chrome.windows.getAll();
   if (windows.length <= 1) {
-    await deleteHistory();
+    await deleteData();
   }
 });
 
-async function deleteHistory() {
+let deleting = false;
+
+async function deleteData() {
+  if (deleting) {
+    return;
+  }
+
+  deleting = true;
+
   try {
-    await chrome.history.deleteAll();
+    await chrome.browsingData.remove(
+      {
+        since: 0,
+      },
+      {
+        appcache: true,
+        cache: true,
+        cacheStorage: true,
+        // cookies: true,
+        downloads: true,
+        fileSystems: true,
+        formData: true,
+        history: true,
+        // indexedDB: true,
+        // localStorage: true,
+        passwords: true,
+        webSQL: true,
+      }
+    );
   } catch (e) {
     console.error(e);
     chrome.windows.create({
       url: "error.html",
       type: "popup",
     });
+  } finally {
+    deleting = false;
   }
 }
